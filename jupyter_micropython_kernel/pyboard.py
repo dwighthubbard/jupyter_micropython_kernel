ad423296918edc24ceb5cebb1b256549fcd1d290
Dwight Hubbard <dhubbard@yahoo-inc.com>
2018-06-19 23:00:46 -0700

    Make sure flow control is disabled

--- jupyter_micropython_kernel/pyboard.py
+++ jupyter_micropython_kernel/pyboard.py
@@ -132,7 +132,7 @@ class Pyboard:
             delayed = False
             for attempt in range(wait + 1):
                 try:
-                    self.serial = serial.Serial(device, baudrate=baudrate, interCharTimeout=1)
+                    self.serial = serial.Serial(device, baudrate=baudrate, interCharTimeout=1, rtscts=False, dsrdtr=False, xonxoff=False)
                     break
                 except (OSError, IOError): # Py2 and Py3 have different errors
                     if wait == 0:
